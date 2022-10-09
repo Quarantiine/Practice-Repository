a524f318e9ccc8a92f9a405eed5de83f2bbd46a0
Daniel Ward <02danwar.buss@gmail.com>
2022-10-09 17:01:38 -0500

    update 1.0.0 unfinish

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -14,7 +14,16 @@ var sideNavbarVar = {
   csbDdContainer: document.querySelector('.csb-dd-container'),
   templateDdContainer: document.querySelector('.template-dd-container'),
   csbDdBox: document.querySelector('.csb-dd-box .expend-more'),
-  templateDdChildren: document.querySelector('.template-dd-children')
+  templateDdChildren: document.querySelector('.template-dd-children'),
+  sideBarSec3Img: document.querySelector('.side-bar-sec-3 img'),
+  sideBarSec3Text: document.querySelector('.side-bar-sec-3 p'),
+  sideNavbarActivitiesPopUp: document.querySelector('.side-navbar-activities-pop-up'),
+  activityBtn: document.querySelector('.activity-btn'),
+  actLink1: document.querySelector('.act-link-1'),
+  pinnedItemsPopUpContainer: document.querySelector('.pinned-items-pop-up-container'),
+  pipucBg: document.querySelector('.pipuc-bg'),
+  pinnedItemsBtn: document.querySelector('.pinned-items-btn'),
+  pipucCloseBtn: document.querySelector('.pipuc-close-btn')
 };
 var sideNavbar = sideNavbarVar.sideNavbar,
     closeSideBar = sideNavbarVar.closeSideBar,
@@ -23,7 +32,16 @@ var sideNavbar = sideNavbarVar.sideNavbar,
     csbDdContainer = sideNavbarVar.csbDdContainer,
     templateDdContainer = sideNavbarVar.templateDdContainer,
     csbDdBox = sideNavbarVar.csbDdBox,
-    templateDdChildren = sideNavbarVar.templateDdChildren;
+    templateDdChildren = sideNavbarVar.templateDdChildren,
+    sideBarSec3Img = sideNavbarVar.sideBarSec3Img,
+    sideBarSec3Text = sideNavbarVar.sideBarSec3Text,
+    sideNavbarActivitiesPopUp = sideNavbarVar.sideNavbarActivitiesPopUp,
+    activityBtn = sideNavbarVar.activityBtn,
+    actLink1 = sideNavbarVar.actLink1,
+    pinnedItemsPopUpContainer = sideNavbarVar.pinnedItemsPopUpContainer,
+    pipucBg = sideNavbarVar.pipucBg,
+    pinnedItemsBtn = sideNavbarVar.pinnedItemsBtn,
+    pipucCloseBtn = sideNavbarVar.pipucCloseBtn;
 var mainBodyContentVar = {
   mainBodyContent: document.querySelector('.main-body-content')
 };
@@ -43,6 +61,7 @@ function () {
         sideNavbar.classList.toggle('side-navbar-class-list');
         closeSideBar.classList.toggle('close-side-bar-class-list');
         mainBodyContent.classList.toggle('main-body-content-cl');
+        sideNavbarActivitiesPopUp.classList.remove('side-navbar-activities-pop-up-cl');
       });
     }
   }, {
@@ -70,6 +89,44 @@ function () {
         templateDdChildren.classList.toggle('template-dd-children-cl');
       });
     }
+  }, {
+    key: "NavBarSection3Method",
+    value: function NavBarSection3Method() {
+      var imgArr = ['https://cdn4.buysellads.net/uu/1/122891/1660240932-ONLINE-STORES_Carbon-Ads_260x200-3.png', 'https://cdn4.buysellads.net/uu/1/122891/1660240905-MEMBER-AREAS_Carbon-Ads_260x200-2.png', 'https://cdn4.buysellads.net/uu/1/122891/1664548801-FreeAcct-NA-Ad3.png', 'https://cdn4.buysellads.net/uu/1/122503/1662488171-MC_Carbon_Logo_260x200.png', 'https://cdn4.buysellads.net/uu/1/122891/1664548579-FreeAcct-Black-Ad2.png', 'https://cdn4.buysellads.net/uu/1/122891/1664548450-MachineLearning-White-Ad4.png', 'https://cdn4.buysellads.net/uu/1/122891/1660240896-CMS_Carbon-Ads_260x200-1.png'];
+      var textArr = ['With online stores, video and scheduling tools, whatever your idea, you can sell anything with Squarespace.', 'Turn your expertise into income by selling access to courses, classes, and community with Squarespace.', 'Start building apps today with 40+ free services and a $200 credit.', 'Get started with the #1 email marketing and automation platform today.', 'Start building apps today with 40+ free services and a $200 credit.', 'Automate machine learning to increase developer productivity with Azure.', 'Squarespace is everything you need to sell anything: your brand, products, services, content, or reservations.'];
+      var ran = Math.floor(Math.random() * imgArr.length);
+      window.addEventListener('load', function () {
+        sideBarSec3Img.src = imgArr[ran];
+        sideBarSec3Text.textContent = textArr[ran];
+      });
+    }
+  }, {
+    key: "SideNavbarActivitiesPopUpMethod",
+    value: function SideNavbarActivitiesPopUpMethod() {
+      activityBtn.addEventListener('click', function () {
+        sideNavbarActivitiesPopUp.classList.toggle('side-navbar-activities-pop-up-cl');
+      });
+    }
+  }, {
+    key: "ActLinkBtnMethod",
+    value: function ActLinkBtnMethod() {
+      actLink1.addEventListener('click', function () {
+        sideNavbarActivitiesPopUp.classList.remove('side-navbar-activities-pop-up-cl');
+      });
+    }
+  }, {
+    key: "PinnedItemsPopUpContainerMethod",
+    value: function PinnedItemsPopUpContainerMethod() {
+      pipucBg.addEventListener('click', function () {
+        pinnedItemsPopUpContainer.classList.remove('pinned-items-pop-up-container-cl');
+      });
+      pinnedItemsBtn.addEventListener('click', function () {
+        pinnedItemsPopUpContainer.classList.add('pinned-items-pop-up-container-cl');
+      });
+      pipucCloseBtn.addEventListener('click', function () {
+        pinnedItemsPopUpContainer.classList.remove('pinned-items-pop-up-container-cl');
+      });
+    }
   }]);
 
   return SideNavigationBar;
@@ -81,6 +138,10 @@ var SNBClassMethodManager = function SNBClassMethodManager() {
   SNB.ClosingSideBarMethod();
   SNB.ExpendCreateBoxMethod();
   SNB.InnerExpendCreateDDBoxMethod();
+  SNB.NavBarSection3Method();
+  SNB.SideNavbarActivitiesPopUpMethod();
+  SNB.ActLinkBtnMethod();
+  SNB.PinnedItemsPopUpContainerMethod();
 };
 
 SNBClassMethodManager();
--- script.js
+++ script.js
@@ -6,12 +6,24 @@ const sideNavbarVar = {
     csbDdContainer: document.querySelector('.csb-dd-container'),
     templateDdContainer: document.querySelector('.template-dd-container'),
     csbDdBox: document.querySelector('.csb-dd-box .expend-more'),
-    templateDdChildren: document.querySelector('.template-dd-children')
+    templateDdChildren: document.querySelector('.template-dd-children'),
+    sideBarSec3Img: document.querySelector('.side-bar-sec-3 img'),
+    sideBarSec3Text: document.querySelector('.side-bar-sec-3 p'),
+    sideNavbarActivitiesPopUp: document.querySelector('.side-navbar-activities-pop-up'),
+    activityBtn: document.querySelector('.activity-btn'),
+    actLink1: document.querySelector('.act-link-1'),
+    pinnedItemsPopUpContainer: document.querySelector('.pinned-items-pop-up-container'),
+    pipucBg: document.querySelector('.pipuc-bg'),
+    pinnedItemsBtn: document.querySelector('.pinned-items-btn'),
+    pipucCloseBtn: document.querySelector('.pipuc-close-btn')
 };
 const {
     sideNavbar, closeSideBar, sideBarSec1, csBoxExpend,
     csbDdContainer, templateDdContainer, csbDdBox,
-    templateDdChildren
+    templateDdChildren, sideBarSec3Img, sideBarSec3Text,
+    sideNavbarActivitiesPopUp, activityBtn, actLink1,
+    pinnedItemsPopUpContainer, pipucBg, pinnedItemsBtn,
+    pipucCloseBtn
 } = sideNavbarVar;
 
 const mainBodyContentVar = {
@@ -29,6 +41,7 @@ class SideNavigationBar {
             sideNavbar.classList.toggle('side-navbar-class-list');
             closeSideBar.classList.toggle('close-side-bar-class-list');
             mainBodyContent.classList.toggle('main-body-content-cl');
+            sideNavbarActivitiesPopUp.classList.remove('side-navbar-activities-pop-up-cl');
         });
     }
 
@@ -54,11 +67,69 @@ class SideNavigationBar {
             templateDdChildren.classList.toggle('template-dd-children-cl');
         });
     }
+
+    NavBarSection3Method() {
+        let imgArr = [
+            'https://cdn4.buysellads.net/uu/1/122891/1660240932-ONLINE-STORES_Carbon-Ads_260x200-3.png',
+            'https://cdn4.buysellads.net/uu/1/122891/1660240905-MEMBER-AREAS_Carbon-Ads_260x200-2.png',
+            'https://cdn4.buysellads.net/uu/1/122891/1664548801-FreeAcct-NA-Ad3.png',
+            'https://cdn4.buysellads.net/uu/1/122503/1662488171-MC_Carbon_Logo_260x200.png',
+            'https://cdn4.buysellads.net/uu/1/122891/1664548579-FreeAcct-Black-Ad2.png',
+            'https://cdn4.buysellads.net/uu/1/122891/1664548450-MachineLearning-White-Ad4.png',
+            'https://cdn4.buysellads.net/uu/1/122891/1660240896-CMS_Carbon-Ads_260x200-1.png'
+        ];
+        let textArr = [
+            'With online stores, video and scheduling tools, whatever your idea, you can sell anything with Squarespace.',
+            'Turn your expertise into income by selling access to courses, classes, and community with Squarespace.',
+            'Start building apps today with 40+ free services and a $200 credit.',
+            'Get started with the #1 email marketing and automation platform today.',
+            'Start building apps today with 40+ free services and a $200 credit.',
+            'Automate machine learning to increase developer productivity with Azure.',
+            'Squarespace is everything you need to sell anything: your brand, products, services, content, or reservations.'
+        ];
+        let ran = Math.floor(Math.random()*imgArr.length);
+
+        window.addEventListener('load', ()=> {
+            sideBarSec3Img.src = imgArr[ran];
+            sideBarSec3Text.textContent = textArr[ran];
+        });
+    }
+
+    SideNavbarActivitiesPopUpMethod() {
+        activityBtn.addEventListener('click', ()=> {
+            sideNavbarActivitiesPopUp.classList.toggle('side-navbar-activities-pop-up-cl');
+        });
+    }
+
+    ActLinkBtnMethod() {
+        actLink1.addEventListener('click', ()=> {
+            sideNavbarActivitiesPopUp.classList.remove('side-navbar-activities-pop-up-cl');
+        });
+    }
+
+    PinnedItemsPopUpContainerMethod() {
+        pipucBg.addEventListener('click', ()=> {
+            pinnedItemsPopUpContainer.classList.remove('pinned-items-pop-up-container-cl');
+        });
+
+        pinnedItemsBtn.addEventListener('click', ()=> {
+            pinnedItemsPopUpContainer.classList.add('pinned-items-pop-up-container-cl');
+        });
+        
+        pipucCloseBtn.addEventListener('click', ()=> {
+            pinnedItemsPopUpContainer.classList.remove('pinned-items-pop-up-container-cl');
+        });
+    }
 }
 const SNB = new SideNavigationBar();
 const SNBClassMethodManager = ()=> {
     SNB.ClosingSideBarMethod();
     SNB.ExpendCreateBoxMethod();
     SNB.InnerExpendCreateDDBoxMethod();
+    SNB.NavBarSection3Method();
+    SNB.SideNavbarActivitiesPopUpMethod();
+    SNB.ActLinkBtnMethod();
+    SNB.PinnedItemsPopUpContainerMethod();
+    
 };
 SNBClassMethodManager();
